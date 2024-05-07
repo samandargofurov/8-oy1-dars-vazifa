a2337ead26fd6e0928049288ae911273b0ca8397
samandargofurov <samandargofurov236@gmail.com>
2024-05-07 21:16:33 +0500

    any linecharts

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import {
   Bar,
   XAxis,
   YAxis,
+  LineChart,
+  Line,
   CartesianGrid,
   Tooltip,
-  Legend
+  Legend,
 } from "recharts";
 
 import BorderColorOutlinedIcon from "@mui/icons-material/BorderColorOutlined";
@@ -23,36 +25,83 @@ import "./App.css";
 import Header from "./components/Header/header";
 
 function App() {
-  const data = [
+  const barData = [
     {
       name: "Page A",
       uv: 4000,
       pv: 7500,
-      amt: 2400
+      amt: 2400,
     },
     {
       name: "Page B",
       uv: 3000,
       pv: 4500,
-      amt: 2210
+      amt: 2210,
     },
     {
       name: "Page C",
       uv: 2000,
       pv: 3000,
-      amt: 2290
+      amt: 2290,
     },
     {
       name: "Page D",
       uv: 2780,
       pv: 5508,
-      amt: 2000
+      amt: 2000,
     },
     {
       name: "Page E",
       uv: 1890,
       pv: 4000,
+      amt: 2181,
+    },
+  ];
+
+
+
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
       amt: 2181
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100
     }
   ];
 
@@ -97,7 +146,7 @@ function App() {
                     <BarChart
                       width={800}
                       height={400}
-                      data={data}
+                      data={barData}
                       margin={{
                         top: 0,
                         right: 30,
@@ -344,7 +393,11 @@ function App() {
                 </div>
 
                 <div className="line-chart">
-                  <div></div>
+                  <div>
+                  <LineChart width={100} height={70} data={data}>
+                    <Line type="monotone" dataKey="pv" stroke="#000" strokeWidth={1} />
+                  </LineChart>
+                  </div>
                   <div className="line-chart-info">
                     <div className="feedbacks">
                       <h1 className="line-chart-order">234 ta</h1>
